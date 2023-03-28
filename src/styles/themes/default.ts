c8b728bc12ee8de0fa1da8dc40a4e77501b84c28
ciceroff <cicero.barrozo13@gmail.com>
2023-03-28 16:56:40 -0300

    🎉 begin project and add theme patterns

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#fff',
+  'base-text': '#574F4D',
+  'base-background': '#FAFAFA',
+  'base-subtitle': '#403937',
+  'base-title': '#272221',
+  'base-card': '#F3F2F2',
+  'base-button': '#E6E5E5',
+  'base-label': '#8D8686',
+  'base-input': '#EDEDED',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'purple-brand': '#8047F8',
+  'purple-light': '#EBE5F9',
+
+  'yellow-brand': '#DBAC2C',
+  'yellow-dark': '#C47F17',
+  'yellow-light': '#F1E9C9',
+}
